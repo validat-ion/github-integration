a931a80c469ef34047d5380374e0d7f19e3bfdb5
Rishichandra Wawhal <rishi@hasura.io>
2021-06-21 21:05:04 +0530

    commit

--- hasura-project/migrations/default/1624211897090_init/up.sql
+++ hasura-project/migrations/default/1624211897090_init/up.sql
@@ -11,5 +11,6 @@ CREATE SEQUENCE public.test_id_seq
     CACHE 1;
 ALTER SEQUENCE public.test_id_seq OWNED BY public.test.id;
 ALTER TABLE ONLY public.test ALTER COLUMN id SET DEFAULT nextval('public.test_id_seq'::regclass);
+
 ALTER TABLE ONLY public.test
     ADD CONSTRAINT test_pkey PRIMARY KEY (id);
